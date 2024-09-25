fa544649cd6e54c92e301e9b10ef323e243bda84
kawajava <kawajava0550@gmail.com>
2024-09-25 23:11:46 +0200

    User microservice added

---
+++ user/src/test/java/kawajava/github/io/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package kawajava.github.io.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
