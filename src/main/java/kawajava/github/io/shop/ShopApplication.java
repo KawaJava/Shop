e4ddc1e7bed58c053078d4fe53fa369dbd1c4b87
kawajava <kawajava0550@gmail.com>
2024-09-25 22:45:51 +0200

    useless dependencies removed

--- src/main/java/kawajava/github/io/shop/ShopApplication.java
+++ src/main/java/kawajava/github/io/shop/ShopApplication.java
@@ -3,9 +3,10 @@ package kawajava.github.io.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class ShopApplication {
 
 	public static void main(String[] args) {
