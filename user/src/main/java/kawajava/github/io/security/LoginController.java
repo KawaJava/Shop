cffed87edb97bf8a4b3e55862dd962a59a049ab6
kawajava <kawajava0550@gmail.com>
2024-09-27 13:28:32 +0200

    JWT Token added

--- user/src/main/java/kawajava/github/io/security/LoginController.java
+++ user/src/main/java/kawajava/github/io/security/LoginController.java
@@ -1,6 +1,9 @@
 package kawajava.github.io.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,11 +12,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
-@RequiredArgsConstructor
 public class LoginController {
 
+    public LoginController(AuthenticationManager authenticationManager,
+                           @Value("${jwt.expirationTime}") long expirationTime,
+                           @Value("${jwt.secret}") String secret) {
+        this.authenticationManager = authenticationManager;
+        this.secret = secret;
+        this.expirationTime = expirationTime;
+    }
+
     private final AuthenticationManager authenticationManager;
+
+    private long expirationTime;
+    private String secret;
 
     @PostMapping("/login")
     public String login(@RequestBody LoginCredentials loginCredentials) {
@@ -22,7 +37,13 @@ public class LoginController {
         );
 
         UserDetails principal = (UserDetails) authenticate.getPrincipal();
-        return String.valueOf(authenticate.isAuthenticated());
+
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+
+        return token;
     }
 
 
