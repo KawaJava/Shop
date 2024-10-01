8c7d89ccc421f43c60a5f9476249d71f18e1ed97
kawajava <kawajava0550@gmail.com>
2024-10-01 14:47:47 +0200

    Kafka and second mikroservice added

---
+++ KafkaExample/src/main/java/kawajava/github/io/kafkaexample/service/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package kawajava.github.io.kafkaexample.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-id")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
